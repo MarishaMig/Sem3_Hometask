5961a671d0283182585f1be2177e36b5f8be6c56
Marina Migurskaya <marina.migurskaya@gmail.com>
2023-12-07 15:13:37 +0300

    Task 1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, 
+значения которых лежат в отрезке [20,90].
+массив [10 21 14 93 23] => 2 */
+Console.Write("Введите размер массива: ");
+int num=Convert.ToInt32(Console.ReadLine());
+int[] array = new int [num];
+Console.Write("массив [");
+int count=0;
+for (int i=0; i<array.Length; i++)
+{
+    array[i] = new Random().Next(1,101);  
+    Console.Write(array[i] +" ");
+    if (array[i]>19 && array[i]<=90)
+    {
+        count++;
+    }
+}
+Console.Write("] => "+count);
